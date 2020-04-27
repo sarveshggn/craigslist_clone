b0c8c3bcc8b42b4d6873c59ba5cb8a0d8015289c
sarvesh joshi <sarveshggn@gmail.com>
2020-04-27 22:00:18 +0530

    did most of the fronted stuff

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = 'my_app'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('new_search/', views.new_search, name='new_search')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,17 @@
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
+
+BASE_CRAIGSLIST_URL = 'https://delhi.craigslist.org/search/hhh?query={}'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
